6fe87cf31ab3a8caf22d17380b25ed28225b7f33
Łukasz Basaj <lbasaj@gmail.com>
2020-02-04 21:03:55 +0100

    Add show password ability, some refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
-const $form = document.getElementById('form');
-const $username = document.getElementById('username');
-const $email = document.getElementById('email');
-const $password = document.getElementById('password');
-const $passwordConfirm = document.getElementById('password-confirm');
-const $passwordFields = document.querySelectorAll(`input[type="password"]`);
+const $form = document.querySelector('.form');
+const $username = document.querySelector('.username');
+const $email = document.querySelector('.email');
+const $password = document.querySelector('.password');
+const $passwordConfirm = document.querySelector('.password-confirm');
 const $fieldIcon = document.querySelectorAll('.field-icon');
 const CONFIG = {
     usernameMinLength: 3,
@@ -30,17 +29,12 @@ function getFieldName(input) {
         return input.id.charAt(0).toUpperCase() + input.id.slice(1);
     } else {
         return input.id.charAt(0).toUpperCase() + input.id.slice(1).replace("-", " ");
-
     }
 }
 
 function checkIfEmailIsProper(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
-        showSuccess(input);
-    } else {
-        showError(input, 'Email is not valid');
-    }
+    return re.test(input.value.trim()) ? showSuccess(input) : showError(input, 'Email is not valid');
 }
 
 function checkFieldLength(input, min, max) {
@@ -67,31 +61,17 @@ function checkPasswordsMatch(input1, input2) {
 
 function checkRequired(inputArr) {
     inputArr.forEach((input) => {
-        if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`)
-        } else {
-            showSuccess(input)
-        }
+        return input.value.trim() === '' ? showError(input, `${getFieldName(input)} is required`) : showSuccess(input);
     })
 }
 
-// function toggleIcon(icon) {
-//     if (icon.classList.contains('fa-eye')) {
-//         icon.classList.replace('fa-eye', 'fa-eye-slash');
-//     } else {
-//         icon.classList.replace('fa-eye-slash', 'fa-eye');
-//     }
-// }
+function toggleIcon($icon) {
+    return $icon.classList.contains('fa-eye') ? $icon.classList.replace('fa-eye', 'fa-eye-slash') : $icon.classList.replace('fa-eye-slash', 'fa-eye');
+}
 
-// function showPassword(input) {
-//     if (input.type === 'password') {
-//         input.type === 'text';
-//         console.log('asdasdsad');
-//     } else {
-//         input.type === 'password';
-//         console.log('asdasdasdasdasdasdas');
-//     }
-// }
+function showPassword(field) {
+    return field.type === "password" ? field.type = "text" : field.type = "password";
+}
 
 function setEventListeners() {
     $form.addEventListener('submit', (evt) => {
@@ -102,17 +82,13 @@ function setEventListeners() {
         checkIfEmailIsProper($email);
         checkPasswordsMatch($password, $passwordConfirm);
     });
-    // fieldIcon.forEach(icon => {
-    //     icon.addEventListener('click', (evt) => {
-    //         toggleIcon(icon);
-    //         passwordFields.forEach(password => {
-    //             password.addEventListener('click', (evt) => {
-    //                 showPassword(password);
-    //             } )
-    //         })
-    //     })
-    // })
-
+    $fieldIcon.forEach($icon => {
+        $icon.addEventListener('click', (evt) => {
+            toggleIcon($icon);
+            showPassword($password);
+            showPassword($passwordConfirm);
+        }, false)
+    })
 }
 
 function main() {
